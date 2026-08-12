40b3d409384c31091d11e685cfb3832cfad3d1e1
Xeno Snow Fox <xenosnowfox@gmail.com>
2026-08-12 18:11:07 +1000

    feat: adding a category class for the different stanza types

---
+++ include/xtrpg/core/StanzaCategory.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace xtrpg {
+namespace core {
+enum class StanzaCategory {
+  /**
+   * A request-response (or query-result) mechanism functioning similarly to
+   * HTTP GET/POST. It permits a client or server to request data, modify
+   * configurations, or fetch metadata like rosters and vCards.
+   */
+  IQ,
+
+  /**
+   * Used to push text or data payloads from one user to another in a
+   * "fire-and-forget" fashion without requiring a direct response.
+   */
+  Message,
+
+  /**
+   * A publish-subscribe mechanism used to broadcast availability status (e.g.,
+   * online, away, offline) or handle contact subscription requests.
+   */
+  Presence
+};
+} // namespace core
+} // namespace xtrpg
