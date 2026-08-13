ab5397e823c26bc38b12751ce1631e015a50fb19
Xeno Snow Fox <xenosnowfox@gmail.com>
2026-08-13 23:49:45 +1000

    Update NodeType.hpp

--- include/xtrpg/xml/NodeType.hpp
+++ include/xtrpg/xml/NodeType.hpp
@@ -4,26 +4,31 @@
 
 namespace xtrpg::xml {
 
-enum class NodeType {
-  /**
-   * Represents a node formatted as an XML tag.
-   */
-  TAG,
+class NodeType {
+public:
+  static const NodeType TAG;
+  static const NodeType TEXT;
 
-  /**
-   * Represents a node that is pure text.
-   */
-  TEXT
+  [[nodiscard]] std::string_view name() const { return this->_name; }
+
+  bool operator==(const NodeType &other) const = default;
+
+  static const std::vector<NodeType> &values() {
+    static const std::vector<NodeType> all = {TAG, TEXT};
+    return all;
+  }
+
+private:
+  constexpr explicit NodeType(std::string_view name) : _name(name) {}
+
+  std::string _name;
 };
 
+inline const NodeType NodeType::TAG{"Tag"};
+inline const NodeType NodeType::TEXT{"Text"};
+
 inline std::ostream &operator<<(std::ostream &os, NodeType type) {
-  switch (type) {
-  case NodeType::TAG:
-    return os << "Tag";
-  case NodeType::TEXT:
-    return os << "Text";
-  }
-  return os << "Unknown";
+  return os << type.name();
 }
 
 } // namespace xtrpg::xml
