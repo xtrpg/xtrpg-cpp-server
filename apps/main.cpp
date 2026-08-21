4b5e84c25b3520f76470b1e0420505165f082d32
Xeno Snow Fox <xenosnowfox@gmail.com>
2026-08-21 15:53:16 +1000

    Add startup banner to server
    Adds a brief startup banner in apps/main.cpp before configuration loading, including application metadata, licensing details, and a warranty notice to identify the server and its open-source terms.

--- apps/main.cpp
+++ apps/main.cpp
@@ -5,6 +5,21 @@
 
 int main(int argc, char *argv[]) {
 
+  // Output preamble information about the application and its configuration
+  // system.
+  std::cout << "XTRPG C++ Server" << std::endl
+            << "Version: 0.1.0" << std::endl
+            << "Copyright (C) 2026 XTRPG Contributors" << std::endl
+            << "License: MIT" << std::endl
+            << "         "
+               "<https://github.com/xtrpg/xtrpg-cpp-server/blob/main/LICENSE>"
+            << std::endl
+            << std::endl
+            << "This is free and open-source software. You are free to use, "
+               "modify, and redistribute it under the terms of the MIT License."
+            << std::endl
+            << "There is NO WARRANTY for this software." << std::endl;
+
   // Discover and register all modules' configuration schemas, then load the
   // configuration file and parse command-line arguments.
   xtrpg::config::ConfigManager configManager;
