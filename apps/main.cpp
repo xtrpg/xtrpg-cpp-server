fdb5b240a256bc6d0ef7d481ece7d6e40171fbbe
Xeno Snow Fox <xenosnowfox@gmail.com>
2026-08-24 16:34:32 +1000

    Use generated version metadata in app
    This updates the app to display the configured project version instead of a hardcoded 0.1.0 value, and includes the generated version header. It also fixes the generated version metadata to use the correct CMake version variables and the xtrpg namespace, while switching the fallback BUILD_VERSION to 0.0.0 so version info stays consistent across builds.

--- apps/main.cpp
+++ apps/main.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #endif
 
+#include "version.hpp"
 #include "xtrpg/config/ConfigManager.hpp"
 
 int main(int argc, char *argv[]) {
@@ -18,20 +19,20 @@ int main(int argc, char *argv[]) {
 
     // Output preamble information about the application and its configuration
     // system.
-    std::cout
-        << "XTRPG: A XMPP Server" << std::endl
-        << "Version: 0.1.0" << std::endl
-        << "Copyright (C) 2026 XTRPG Contributors" << std::endl
-        << "License: MIT" << std::endl
-        << "         "
-           "<https://github.com/xtrpg/xtrpg-cpp-server/blob/main/LICENSE>"
-        << std::endl
-        << std::endl
-        << "This is free and open-source software. You are free to use, "
-           "modify, and redistribute it under the terms of the MIT License."
-        << std::endl
-        << "There is NO WARRANTY for this software." << std::endl
-        << std::endl;
+    std::cout << "XTRPG: A XMPP Server" << std::endl
+              << "Version: " << xtrpg::VERSION << std::endl
+              << "Copyright (C) 2026 XTRPG Contributors" << std::endl
+              << "License: MIT" << std::endl
+              << "         "
+                 "<https://github.com/xtrpg/xtrpg-cpp-server/blob/main/LICENSE>"
+              << std::endl
+              << std::endl
+              << "This is free and open-source software." << std::endl
+              << "You are free to use, modify, and redistribute it under the "
+                 "terms of the MIT License."
+              << std::endl
+              << "There is NO WARRANTY for this software." << std::endl
+              << std::endl;
 
     // Discover and register all modules' configuration schemas, then load the
     // configuration file and parse command-line arguments.
