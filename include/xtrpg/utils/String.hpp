11bf8ea73a0ddc6fd05083c0805dc6f8bf63b6d7
Xeno Snow Fox <xenosnowfox@gmail.com>
2026-09-07 15:44:06 +1000

    Make string utilities inline; add isBlank
    Add isBlank(const std::string&) and include <cctype>. Mark ltrim, rtrim, trim, toLowerCase, and countUtf8CodePoints as inline to prevent multiple-definition/ODR issues when the header is included in multiple translation units. Minor formatting changes in include/xtrpg/utils/String.hpp.

--- include/xtrpg/utils/String.hpp
+++ include/xtrpg/utils/String.hpp
@@ -1,27 +1,38 @@
 #pragma once
 
 #include <algorithm>
+#include <cctype>
 #include <string>
 
 namespace xtrpg::utils::string {
+inline bool isBlank(const std::string &s) {
+  return std::all_of(s.begin(), s.end(), [](unsigned char character) {
+    return std::isspace(character) != 0;
+  });
+}
+
 // Trim from start (in-place)
-void ltrim(std::string &s) { s.erase(0, s.find_first_not_of(" \t\n\r\f\v")); }
+inline void ltrim(std::string &s) {
+  s.erase(0, s.find_first_not_of(" \t\n\r\f\v"));
+}
 
 // Trim from end (in-place)
-void rtrim(std::string &s) { s.erase(s.find_last_not_of(" \t\n\r\f\v") + 1); }
+inline void rtrim(std::string &s) {
+  s.erase(s.find_last_not_of(" \t\n\r\f\v") + 1);
+}
 
 // Trim from both ends (in-place)
-void trim(std::string &s) {
+inline void trim(std::string &s) {
   rtrim(s);
   ltrim(s);
 }
 
-void toLowerCase(std::string &s) {
+inline void toLowerCase(std::string &s) {
   std::transform(s.begin(), s.end(), s.begin(),
                  [](unsigned char c) { return std::tolower(c); });
 }
 
-size_t countUtf8CodePoints(const std::string &str) {
+inline size_t countUtf8CodePoints(const std::string &str) {
   size_t count = 0;
   for (unsigned char c : str) {
     // If the byte is NOT a UTF-8 continuation byte, it's the start of a
